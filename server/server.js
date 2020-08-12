e796303d3b37c82cf867175dfbd5b21346211a65
Daniel Vickers <dnlvickers5@gmail.com>
2020-08-12 14:03:24 -0400

    fixed game logic

--- server/server.js
+++ server/server.js
@@ -1,39 +1,47 @@
-//required for runnngin the server
+//needed packets
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 
-//create the app to use express, and find static files
+//create express app and find static files
 const app = express();
 const clientPath  = __dirname+'/../client';
 console.log('Serving static from '+clientPath)
-
-//wrap app into server and in socket.io
 app.use(express.static(clientPath));
+
+//wrap app in server and io
 const server = http.createServer(app);
 const io = socketio(server);
 
-//define global as the waiting player for a game
+server.on('error', function(err){
+	console.error('Server error:',err);
+});
+
+server.listen(8080, function() {
+	console.log('Program Init on 8080');
+});
+
+//global for socket of player waiting to play
 let waitingPlayer=null;
 
 io.on('connection', function(sock){
-	//handshake with the static page
+	//shake hands with the client side
 	sock.emit('message','You connected');
 	console.log('Someone connected');
 
-	//if the waiting player is defined, then begin a match
+	//checks if we already have a wiating player to create a game pair
 	if (waitingPlayer) {
 		console.log('Starting Game');
 		new Connect4Game(waitingPlayer,sock);
 		waitingPlayer = null;
 
-		//otherwise set this new player as the waiting player
+	//otherwise set the new player to the waiting player
 	} else {
 		waitingPlayer = sock;
 		sock.emit('message','Waiting for opponent');
 	}
 
-	//on disconnect, if the disconnect was the waiting player, reset waiting player
+	//if the waiting player disconnects, reset the waiting player socket
 	sock.on('disconnect',function(){
 		if (sock==waitingPlayer) {
 			waitingPlayer = null;
@@ -41,20 +49,10 @@ io.on('connection', function(sock){
 	})
 })
 
-//error message from server
-server.on('error', function(err){
-	console.error('Server error:',err);
-});
-
-//port to listen for server
-server.listen(8080, function() {
-	console.log('Program Init on 8080');
-});
-
-//class to create and run an instance of the game
+//create class to create and run a game instance
 class Connect4Game {
 	constructor(player1,player2) {
-		//necessary constructor elements for the game
+		//necessary class elemetns for runngint he game
 		this._players=[player1,player2];
 		this._board=this.new_board();
 		this._players.forEach(s => s.emit('message','Opponent found'));
@@ -66,15 +64,16 @@ class Connect4Game {
 		//check for player inputs
 		this._players.forEach((player,ind) => {
 			player.on('move',(col) => {
-				//if the game is still running, try making the move the player makes
+				//check if the game is over and if we should register the move
 				if(!this._over){
+					//attempt to make the move the player input
 					this.make_move(player,ind,col);
 				}
 			});
 		})
 	}
 
-	//generate the server-side game board
+	//generates a new 7x6 game board on new game
 	new_board(){
 		var board = [];
 		for (var i = 0; i < 7; i++) {
@@ -87,17 +86,17 @@ class Connect4Game {
 		return board;
 	}
 
+	//checks to make the player's attempted move
 	make_move(player,ind,col) {
 		//check that it is that player's turn
 		if (ind == this._turn%2.0) {
 			//check that the column is not full
 			if (this._board[col][5]==2) {
-				//if not full, add piece to the column
+				//go through with adding the game piece tot he column
 				this.add_piece(col);
-				//send updates to board
+				//emits updates game board
 				this._players.forEach(s => s.emit('board',this._board));
-
-				//if game is not over, let the players know whose turn it is
+				//if the game is not over, let players know whose turn it is
 				if (!this._over) {
 					player.emit('message','You made a move')
 					this._players[this._turn % 2].emit('message','Your turn');
@@ -110,9 +109,11 @@ class Connect4Game {
 		}
 	}
 
+	//adds game piece to desired colimn
 	add_piece(col){
-		//iterate throgh the board and add piece to first empty slot
+		//iterate throgh the board and add piece to first empty square
 		for (var i = 0; i < this._board[col].length; i++) {
+			//checks for the first empty square
 			if (this._board[col][i] == 2){
 				this._board[col][i] = this._turn % 2;
 				this.check_gameover(col,i);
@@ -122,43 +123,50 @@ class Connect4Game {
 		}
 	}
 
-	check_gameover(col,row) {
+	check_gameover(i,j) {
 		//enter end game logic
-		var pieceValue = this._board[col][row];
-		//if vertical row
-		if (row > 2) {
-			if (pieceValue == this._board[col][row-1] && pieceValue == this._board[col][row-2] && pieceValue == this._board[col][row-3]) {
-				this._winner = pieceValue;
+		var pieceValue = this._board[i][j];
+		for (var col = 0; col < 7; col++) {
+			for (var row = 0; row < 6; row++) {
+				//only player who can win is the one who makes the move. 
+				if (pieceValue == this._board[row][col]) {}
+					//if vertical row
+					if (row > 2) {
+						if (pieceValue == this._board[col][row-1] && pieceValue == this._board[col][row-2] && pieceValue == this._board[col][row-3]) {
+							this._winner = pieceValue;
+						}
+					}
+					if (col < 4) {
+						//if horizontal right
+						if (pieceValue == this._board[col+1][row] && pieceValue == this._board[col+2][row] && pieceValue == this._board[col+3][row]) {
+							this._winner = pieceValue;
+						} else if (row < 3) {
+							//if diagonal up-right
+							if (pieceValue == this._board[col+1][row+1] && pieceValue == this._board[col+2][row+2] && pieceValue == this._board[col+3][row+3]) {
+								this._winner = pieceValue;
+							}
+							//if diagonal down-right
+						} else if (pieceValue == this._board[col+1][row-1] && pieceValue == this._board[col+2][row-2] && pieceValue == this._board[col+3][row-3]) {
+							this._winner = pieceValue;
+						}
+					}
+					if (col > 2) {
+						//if horizontal left
+						if (pieceValue == this._board[col-1][row] && pieceValue == this._board[col-2][row] && pieceValue == this._board[col-3][row]) {
+							this._winner = pieceValue;
+						} else if (row < 3) {
+							//if diagonal up-left
+							if (pieceValue == this._board[col-1][row+1] && pieceValue == this._board[col-2][row+2] && pieceValue == this._board[col-3][row+3]) {
+								this._winner = pieceValue;
+							}
+							//if diagonal down-left
+						} else if (pieceValue == this._board[col-1][row-1] && pieceValue == this._board[col-2][row-2] && pieceValue == this._board[col-3][row-3]) {
+							this._winner = pieceValue;
+						}
+					}
 			}
 		}
-		if (col < 4) {
-			//if horizontal right
-			if (pieceValue == this._board[col+1][row] && pieceValue == this._board[col+2][row] && pieceValue == this._board[col+3][row]) {
-				this._winner = pieceValue;
-			} else if (row < 3) {
-				//if diagonal up-right
-				if (pieceValue == this._board[col+1][row+1] && pieceValue == this._board[col+2][row+2] && pieceValue == this._board[col+3][row+3]) {
-					this._winner = pieceValue;
-				}
-				//if diagonal down-right
-			} else if (pieceValue == this._board[col+1][row-1] && pieceValue == this._board[col+2][row-2] && pieceValue == this._board[col+3][row-3]) {
-				this._winner = pieceValue;
-			}
-		}
-		if (col > 2) {
-			//if horizontal left
-			if (pieceValue == this._board[col-1][row] && pieceValue == this._board[col-2][row] && pieceValue == this._board[col-3][row]) {
-				this._winner = pieceValue;
-			} else if (row < 3) {
-				//if diagonal up-left
-				if (pieceValue == this._board[col-1][row+1] && pieceValue == this._board[col-2][row+2] && pieceValue == this._board[col-3][row+3]) {
-					this._winner = pieceValue;
-				}
-				//if diagonal down-left
-			} else if (pieceValue == this._board[col-1][row-1] && pieceValue == this._board[col-2][row-2] && pieceValue == this._board[col-3][row-3]) {
-				this._winner = pieceValue;
-			}
-		}
+		
 		//if we have a winner, let players know that the game is over
 		if (this._winner == 0 || this._winner == 1) {
 			this._players[pieceValue].emit('message','Congratualtions. You won!');
